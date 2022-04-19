7c80d1e6e8111940b1fd08c53f4204d14c394c40
duydo8 <duydogch123456@gmail.com>
2022-04-19 13:52:45 +0700

    update pom and define a function in AccountRepository

---
+++ src/main/java/com/bkav/demo/custom/CustomUserDetails.java
@@ -0,0 +1,5 @@
+package com.bkav.demo.custom;
+
+public class CustomUserDetails {
+
+}
--- src/main/java/com/bkav/demo/repository/AccountRepository.java
+++ src/main/java/com/bkav/demo/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package com.bkav.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bkav.demo.entities.Accounts;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Accounts, String>{
-
+    @Query("select a from Accounts  a where a.username=?1")
+    Accounts findByUsername(String username);
 }
